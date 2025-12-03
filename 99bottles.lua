3874c536e7c16b7a6aaf373fa46300940bec719b
NicoPlayZ9002-YT <ngestal9000@gmail.com>
2025-12-03 23:36:49 +0100

    Create 99bottles.lua

---
+++ 99bottles.lua
@@ -0,0 +1,52 @@
+-- 99bottles.trs, program-table-fied
+program = {
+  "INT N 98",
+  "INT ONE 1",
+  "INT C 0",
+  "LIT T \"Take one down, pass it around,\"",
+    
+  "WHL N",
+  "MOV C N",
+  "ADD C ONE",
+  "LIT A \"\"",
+  "CCT A C",
+  "LIT B \" bottles of beer on the wall,\"",
+  "CCT A B",
+  "PRN A",
+
+  "LIT A \"\"",
+  "CCT A C",
+  "LIT B \" bottles of beer!\"",
+  "CCT A B",
+  "PRN A",
+
+  "PRN T",
+
+  "SUB N ONE",
+
+  "JEZ N 28",
+
+  "LIT A \"\"",
+  "CCT A C",
+  "LIT B \" bottles of beer on the wall! \n \"",
+  "CCT A B",
+  "PRN A",
+
+  "JMP 3",
+
+  "ENW",
+
+  "LIT A \"1 bottle of beer on the wall! \n \"",
+  "PRN A",
+
+  "LIT A \"1 bottle of beer on the wall,\"",
+  "PRN A",
+
+  "LIT A \"1 bottle of beer!\"",
+  "PRN A",
+
+  "PRN T",
+  
+  "LIT A \"No more bottles of beer on the wall!\"",
+  "PRN A"
+}
