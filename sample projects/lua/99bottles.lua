3cbd2ca0c3e1a5306fcbaab5cd8ccf20110b736d
NicoPlayZ9002-YT <ngestal9000@gmail.com>
2025-12-05 17:25:46 +0100

    Update 99bottles.lua

--- sample projects/lua/99bottles.lua
+++ sample projects/lua/99bottles.lua
@@ -1,52 +1,52 @@
 -- 99bottles.trs, program-table-fied
 program = {
-  "INT N 98",
-  "INT ONE 1",
-  "INT C 0",
-  "LIT T \"Take one down, pass it around,\"",
+  'INT N 98',
+  'INT ONE 1',
+  'INT C 0',
+  'LIT T "Take one down, pass it around,"',
     
-  "WHL N",
-  "MOV C N",
-  "ADD C ONE",
-  "LIT A \"\"",
-  "CCT A C",
-  "LIT B \" bottles of beer on the wall,\"",
-  "CCT A B",
-  "PRN A",
+  'WHL N',
+  'MOV C N',
+  'ADD C ONE',
+  'LIT A ""',
+  'CCT A C',
+  'LIT B " bottles of beer on the wall,"',
+  'CCT A B',
+  'PRN A',
 
-  "LIT A \"\"",
-  "CCT A C",
-  "LIT B \" bottles of beer!\"",
-  "CCT A B",
-  "PRN A",
+  'LIT A ""',
+  'CCT A C',
+  'LIT B " bottles of beer!"',
+  'CCT A B',
+  'PRN A',
 
-  "PRN T",
+  'PRN T',
 
-  "SUB N ONE",
+  'SUB N ONE',
 
-  "JEZ N 28",
+  'JEZ N 28',
 
-  "LIT A \"\"",
-  "CCT A C",
-  "LIT B \" bottles of beer on the wall! \n \"",
-  "CCT A B",
-  "PRN A",
+  'LIT A ""',
+  'CCT A C',
+  'LIT B " bottles of beer on the wall! \n "',
+  'CCT A B',
+  'PRN A',
 
-  "JMP 3",
+  'JMP 3',
 
-  "ENW",
+  'ENW',
 
-  "LIT A \"1 bottle of beer on the wall! \n \"",
-  "PRN A",
+  'LIT A "1 bottle of beer on the wall! \n "',
+  'PRN A',
 
-  "LIT A \"1 bottle of beer on the wall,\"",
-  "PRN A",
+  'LIT A "1 bottle of beer on the wall,"',
+  'PRN A',
 
-  "LIT A \"1 bottle of beer!\"",
-  "PRN A",
+  'LIT A "1 bottle of beer!"',
+  'PRN A',
 
-  "PRN T",
+  'PRN T',
   
-  "LIT A \"No more bottles of beer on the wall!\"",
-  "PRN A"
+  'LIT A "No more bottles of beer on the wall!"',
+  'PRN A'
 }
